e980a2372f66841f7e2dadacaeba5cf39a2c9f1d
Claire_M <clairerodriguez79@yahoo.fr>
2023-03-23 14:21:52 +0100

    EmptyList & EmailInput with new syntax (in comment)

--- shiny/src/components/EmailInput/Index.jsx
+++ shiny/src/components/EmailInput/Index.jsx
@@ -53,3 +53,42 @@ class EmailInput extends Component {
 }
 
 export default EmailInput
+
+//With new syntax::
+
+// const InputWrapper = styled.div`
+//   color: ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+//   display: flex;
+//   flex-direction: column;
+// `
+
+// const StyledLabel = styled.label`
+//   color: ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+// `
+
+// const StyledInput = styled.input`
+//   border: none;
+//   color: ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+//   background-color: transparent;
+//   border-bottom: 1px solid
+//     ${({ theme }) => (theme === 'light' ? colors.dark : 'white')};
+//   margin-top: 5px;
+//   margin-bottom: 15px;
+// `
+
+// function EmailInput({ theme }) {
+//   const [inputValue, setInputValue] = useState('')
+
+//   return (
+//     <InputWrapper theme={theme}>
+//       <StyledLabel theme={theme}>Adresse email</StyledLabel>
+//       <StyledInput
+//         theme={theme}
+//         onChange={(e) => setInputValue(e.target.value)}
+//       />
+//       {inputValue}
+//     </InputWrapper>
+//   )
+// }
+
+// export default EmailInput
